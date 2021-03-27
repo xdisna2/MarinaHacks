36268162946d2bb42507b6724f7def5dc37c9f29
dolan77 <dylanhuynh77@gmail.com>
2021-03-27 13:09:54 -0700

    added a method that display's the player's turn.

--- main.py
+++ main.py
@@ -2,24 +2,30 @@ import pygame
 import display
 pygame.init()
 
+# 0 = O's turn | 1 = X's turn
 def main():
-    mouse = pygame.mouse.get_pos()
+
     # game loop, terminates when the game ends
     run = True
 
+
     while run:
+        mouse = pygame.mouse.get_pos()
+        print(mouse)
         # gets a list of all the events and loops through them
         for event in pygame.event.get():
             # if the user exits the window, terminate the game
             if event.type == pygame.QUIT:
                 run = False
 
+            # code
+
         display.bg_color()
         display.grid()
         display.hud()
         display.display_x()
         display.display_o()
-
+        display.display_turn(1)
 
         pygame.display.update()
 
